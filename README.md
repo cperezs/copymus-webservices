6260273e934d135c9009fadd57684f4bcfea15b2
Carlos Pérez <cperez@dlsi.ua.es>
2018-02-27 16:07:23 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# copymus-webservices
---
+++ src/main/java/es/ua/dlsi/grfia/hispamus/CopymusWebservicesApplication.java
@@ -0,0 +1,12 @@
+package es.ua.dlsi.grfia.hispamus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopymusWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopymusWebservicesApplication.class, args);
+	}
+}
